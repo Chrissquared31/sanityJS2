42668d6dea710f3b0900cdc55a6cce1a18d670af
Chrissquared31 <cnc9@njit.edu>
2023-04-20 12:26:14 -0400

    fixes to images

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,8 @@
-// Re-export `NextStudioHead` as default if you're happy with the default behavior
-export {NextStudioHead} from 'next-sanity/studio/head'
-
-// To customize it, use it as a children component:
-import {NextStudioHead} from 'next-sanity/studio/head'
+import { NextStudioHead } from "next-sanity/studio/head";
 
 export default function CustomStudioHead() {
+  const basePath = process.env.BASE_PATH || "";
+
   return (
     <>
       <NextStudioHead favicons={false} />
@@ -14,6 +12,14 @@ export default function CustomStudioHead() {
         sizes="32x32"
         href="https://www.sanity.io/static/images/favicons/favicon-32x32.png"
       />
+      <style jsx global>{`
+        .header-background::before {
+          background-image: url(${basePath}/nelo_classroom.png);
+        }
+      `}</style>
     </>
-  )
+  );
 }
+
+
+
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -5,13 +7,19 @@ const nextConfig = {
   },
   images: {
     domains: ["cdn.sanity.io"],
+    unoptimized: true, // Add this line
   },
   env: {
     NEXT_PUBLIC_MAILCHIMP_API_KEY: process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY,
     NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID: process.env.NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID,
+    BASE_PATH: isProd ? '/sanityJS2' : '', // Add this line
   },
-  output: 'export',
+
+  output: {
+    export: true,
+  },
 };
 
 module.exports = nextConfig;
+
 
